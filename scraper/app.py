436cf0cfab6d3c3adb86251e94606ea6ba5f8286
spencer <spencer@mediaexcel.com>
2023-04-22 22:48:42 -0400

    Changed to create a separate process to avoid twisted reactor non-restartable error.

--- scraper/app.py
+++ scraper/app.py
@@ -4,17 +4,33 @@ from scraper.spiders.amazon import AmazonSpider
 
 import asyncio
 import nest_asyncio
+
 nest_asyncio.apply()
 
 from fastapi import FastAPI
+from multiprocessing import Process, Queue
+
 
 from scraper.amazon import Amazon
 
 app = FastAPI()
 
+results = {}
+
 
 @app.get("/", tags = ['ROOT'])
 async def root () -> list:
+    q = Queue()
+    p = Process(target = scrape_amazon, args = (q,))
+    p.start()
+    p.join()
+    res = q.get()
+    q.close()
+    q.join_thread()
+    return res
+
+
+def scrape_amazon (queue) -> list:
     keyword = 'tmg'
     if len(keyword) < 1:
         keyword = 'vitamin c'
@@ -23,8 +39,5 @@ async def root () -> list:
     data = []
     process.crawl(AmazonSpider, keyword = keyword, data = data)
     process.start()
-    print(data)
-    
-    
-    # result = await Amazon.scrape('zinc')
+    queue.put(data)
     return data
