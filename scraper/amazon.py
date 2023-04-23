4fedfe29cd4338d77555e1e668093a040ef80955
spencer <spencer@mediaexcel.com>
2023-04-22 23:06:49 -0400

    extracted crawling method out to amazon class.

--- scraper/amazon.py
+++ scraper/amazon.py
@@ -1,18 +1,32 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from scraper.spiders.amazon import AmazonSpider
+from multiprocessing import Process, Queue
 
 
 class Amazon:
     
     @staticmethod
-    async def scrape (keyword):
+    def scrape(keyword) -> list:
         if len(keyword) < 1:
             keyword = 'vitamin c'
-        
-        process = CrawlerProcess(get_project_settings())
-        data = []
-        process.crawl(AmazonSpider, keyword = keyword, data = data)
-        await process.start()
-        print(data)
-        return data
+            
+        q = Queue()
+        q.put(keyword)
+        p = Process(target = crawl, args = (q,))
+        p.start()
+        p.join()
+        res = q.get()
+        q.close()
+        q.join_thread()
+        return res
+
+
+def crawl(queue) -> list:
+    keyword = queue.get()
+    process = CrawlerProcess(get_project_settings())
+    data = []
+    process.crawl(AmazonSpider, keyword = keyword, data = data)
+    process.start()
+    queue.put(data)
+    return data
--- scraper/app.py
+++ scraper/app.py
@@ -1,43 +1,12 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-from scraper.spiders.amazon import AmazonSpider
+from scraper.amazon import Amazon
+from fastapi import FastAPI
 
-import asyncio
 import nest_asyncio
 
 nest_asyncio.apply()
-
-from fastapi import FastAPI
-from multiprocessing import Process, Queue
-
-
-from scraper.amazon import Amazon
-
 app = FastAPI()
-
-results = {}
 
 
 @app.get("/", tags = ['ROOT'])
-async def root () -> list:
-    q = Queue()
-    p = Process(target = scrape_amazon, args = (q,))
-    p.start()
-    p.join()
-    res = q.get()
-    q.close()
-    q.join_thread()
-    return res
-
-
-def scrape_amazon (queue) -> list:
-    keyword = 'tmg'
-    if len(keyword) < 1:
-        keyword = 'vitamin c'
-    
-    process = CrawlerProcess(get_project_settings())
-    data = []
-    process.crawl(AmazonSpider, keyword = keyword, data = data)
-    process.start()
-    queue.put(data)
-    return data
+async def root() -> list:
+    return Amazon.scrape('iron')
