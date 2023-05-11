b2a0b53369f940c9553c29cc3bcc7382d1e552cf
spencer <spencer@mediaexcel.com>
2023-05-11 00:47:51 -0400

    Rate limit request to amazon to 1 request per 60 seconds.

--- scraper/app.py
+++ scraper/app.py
@@ -1,5 +1,7 @@
 import json
 import os.path
+import time
+
 from amazon import Amazon
 from fastapi import FastAPI
 import nest_asyncio
@@ -8,6 +10,9 @@ nest_asyncio.apply()
 app = FastAPI()
 base_storage = '/tmp/search/'
 isExist = os.path.exists(base_storage)
+
+app.last_amazon_request = time.time()
+
 if not isExist:
     os.makedirs(base_storage)
     print("The new directory is created!")
@@ -21,7 +26,7 @@ async def root() -> dict:
 @app.get("/supplement/{keyword}")
 async def supplement_search(keyword: str, update_cache: bool = False, country_code: str = 'jp') -> dict:
     # return cached value if exists
-    file_path = base_storage + keyword
+    file_path = base_storage + keyword + '_' + country_code
     try:
         if os.path.isfile(file_path) and update_cache is False:
             f = open(file_path)
@@ -31,9 +36,20 @@ async def supplement_search(keyword: str, update_cache: bool = False, country_co
         os.remove(file_path)
         return {'data': IOError, 'success': False}
     
+    # rate limit request to amazon
+    # only 1 request can be processed per time window, which is the same as the cooldown
+    request_cooldown = 60  # seconds
+    sec_since_last_req = time.time() - app.last_amazon_request
+    if sec_since_last_req < request_cooldown:
+        return {'data': [],
+                'success': False,
+                'message': '%d seconds until a new request can be processed' %
+                           (request_cooldown - sec_since_last_req)}
+    
     # search
     try:
         result = Amazon.scrape(keyword, country_code)
+        app.last_amazon_request = time.time()
     except Exception:
         return {'data': Exception, 'success': False}
     
